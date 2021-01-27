1219941cdf065a0564b4d011aff3374fe08dfcbc
John Kearney <John.d.kearney1@gmail.com>
2021-01-26 23:34:25 -0600

    created users for user seeder scrypt

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+//Remember, that data we create here in the data folder must match the models we created in our 'models' folder or mongoose won't let us use it in our db
+
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@gofar.com',
+    password: bcrypt.hashSync('12345', 10), //normally this would best be done asynchronously, but it's fine here synchronously w/ just 3 users
+    isAdmin: true,
+    wishList: []
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    password: bcrypt.hashSync('12345', 10),
+    wishList: []
+  },
+  {
+    name: 'Jane Doe',
+    email: 'janedoe@gmail.com',
+    password: bcrypt.hashSync('12345', 10),
+    wishList: []
+  }
+];
+
+export default users;
