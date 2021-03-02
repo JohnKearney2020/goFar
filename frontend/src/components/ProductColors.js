fd80b6ba6e9c1b2fb308c06f01cdcda6d908a74d
John Kearney <John.d.kearney1@gmail.com>
2021-03-01 20:25:54 -0600

    got an active border working on color buttons on the product screen when clicked. Used uuid() to help in this

--- frontend/src/components/ProductColors.js
+++ frontend/src/components/ProductColors.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Container, Image, Row } from 'react-bootstrap';
+// import { Container, Image, Row } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
 
 import './ProductColors.css';
 
@@ -7,17 +9,36 @@ const ProductColors = ({ images, colorSelectHandler }) => {
   //this function is defined in the ProductCard component
   const newColorClickedHandler = (e) => {
     colorSelectHandler(e.target.dataset.color); //its expecting a color name as a string, ex: 'seapine'
+    const colorsButtons = document.getElementsByClassName('colorButton');
+    // colorsButtons.forEach(eachImage => eachImage.classList.remove('colorActiveBorder')); 
+    //remove any existing active borders
+    for(let eachColor of colorsButtons) {
+      eachColor.classList.remove('colorButtonActiveBorder');
+    }
+    const activeColor = document.getElementById(e.target.id);
+    activeColor.classList.add('colorButtonActiveBorder')
+
+    // remove existing borders
+    // const imagesWithBorders = document.getElementsByClassName('productDetailsCarouselImage');
+    // for(let eachImage of imagesWithBorders){
+    //   eachImage.classList.remove('selectedBorderCarousel');
+    // }
+    // Add the border to the clicked image
+    // const imageForBorder = document.getElementById(e.target.id);
+    // imageForBorder.classList.add('selectedBorderCarousel');
+
   }
 
   return (
     // <Container>
       // <Row>
       <>
-        {images.map(eachColorImage => (
-          <Image src={eachColorImage.tinyImage} roundedCircle thumbnail className="mr-1 productColorsBorder" 
+        {images.map((eachColorImage,idx) => (
+          <Image src={eachColorImage.tinyImage} roundedCircle thumbnail className="mr-1 colorButton" 
           key={eachColorImage.tinyImage}
           data-color={eachColorImage.colorName}
           onClick={newColorClickedHandler}
+          id={uuidv4()}
           />
         ))}
       </>
