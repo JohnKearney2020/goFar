1a0f0f5960a45bc4b03e911b27540a424c69b5b5
John Kearney <John.d.kearney1@gmail.com>
2021-03-08 17:06:42 -0600

    authorization middleware working. Login POST route done and user Profile GET route done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc     Auth user & get token
 // @route    POST /api/users/login
@@ -15,7 +16,9 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      cart: user.cart,
+      wishList: user.wishList,
+      token: generateToken(user._id)
     })
   } else {
     res.status(401); // status for unauthorized
@@ -23,4 +26,25 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser };
+
+// @desc     Get user profile
+// @route    GET /api/users/profile
+// @access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      cart: user.cart,
+      wishList: user.wishList,
+    })
+  } else {
+    res.status(404); //not found
+    throw new Error('Invalid email or password');
+  }
+})
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 // Root url: /api/users
 router.post('/login', authUser);
-
+// Protected Routes
+router.route('/profile').get(protect, getUserProfile);
 export default router;
