defd89e8149a53ce51b6b38853702090d55179bc
John Kearney <John.d.kearney1@gmail.com>
2021-03-11 22:23:18 -0600

    made a good looking user profile heading. Can probably make it a component and reuse it. Also need some mobile responsiveness

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, Card, ListGroup } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 // import FormContainer from '../components/FormContainer';
 import { getUserDetails } from '../actions/userActions';
+import './ProfileScreen.css';
 
 const ProfileScreen = ({ history }) => {
 
@@ -49,19 +50,19 @@ const ProfileScreen = ({ history }) => {
     e.preventDefault();
     let anyErrors = false;
     //Clear any existing errors messages first
-    // if(nameMessage) { setNameMessage(null) }
-    // if(emailMessage) { setEmailMessage(null) }
-    // if(passwordMessage) { setPasswordMessage(null) }
-    // if(confirmPasswordMessage) { setConfirmPasswordMessage(null) }
+    if(nameMessage) { setNameMessage(null) }
+    if(emailMessage) { setEmailMessage(null) }
+    if(passwordMessage) { setPasswordMessage(null) }
+    if(confirmPasswordMessage) { setConfirmPasswordMessage(null) }
     //Check for blank fields next
-    // if(name === ''){ 
-    //   setNameMessage('Name field cannot be empty');
-    //   anyErrors = true;
-    // }
-    // if(email === ''){
-    //   setEmailMessage('Email field cannot be empty');
-    //   anyErrors = true;
-    // }
+    if(name === ''){ 
+      setNameMessage('Name field cannot be empty');
+      anyErrors = true;
+    }
+    if(email === ''){
+      setEmailMessage('Email field cannot be empty');
+      anyErrors = true;
+    }
     // if(password === ''){
     //   setPasswordMessage('Password field cannot be empty');
     //   anyErrors = true;
@@ -70,82 +71,98 @@ const ProfileScreen = ({ history }) => {
     //   setConfirmPasswordMessage('Confirm password field cannot be empty');
     //   anyErrors = true;
     // }
-    // if(password !== confirmPassword) {
-    //   setPasswordMessage('Your passwords do not match');
-    //   setConfirmPasswordMessage('Your passwords do not match');
-    //   anyErrors = true;
-    // }
+    if(password !== confirmPassword) {
+      setPasswordMessage('Your passwords do not match');
+      setConfirmPasswordMessage('Your passwords do not match');
+      anyErrors = true;
+    }
 
-    // if(anyErrors) { return }
-    // dispatch(register(name, email, password));
-
+    if(anyErrors) { return }
+    console.log('dispatch placeholder')
     //DISPATCH UPDATE PROFILE
   }
 
   return (
-    <Row>
-      <Col md={3}>
-        <h2>User Profile</h2>
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId='name'>
-            <Form.Label>Name</Form.Label>
-            <Form.Control 
-              type='name' 
-              // placeholder='Enter name'
-              placeholder='Enter name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              // className='is-invalid'
-              className={nameMessage === null ? '' : 'is-invalid'}
-            >
-            </Form.Control>
-            { nameMessage && <div class="invalid-feedback">{nameMessage}</div> }
-          </Form.Group>
-          <Form.Group controlId='email'>
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control 
-              type='email' 
-              placeholder='Enter email address' 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={emailMessage === null ? '' : 'is-invalid'}
-            >
-            </Form.Control>
-            { emailMessage && <div class="invalid-feedback">{emailMessage}</div> }
-          </Form.Group>
-          <Form.Group controlId='password'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control 
-              type='password' 
-              placeholder='Enter password' 
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={passwordMessage === null ? '' : 'is-invalid'}
-            >
-            </Form.Control>
-            { passwordMessage && <div class="invalid-feedback">{passwordMessage}</div> }
-          </Form.Group>
-          <Form.Group controlId='confirmPassword'>
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control 
-              type='password' 
-              placeholder='Confirm password' 
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className={confirmPasswordMessage === null ? '' : 'is-invalid'}
-            >
-            </Form.Control>
-            { confirmPasswordMessage && <div class="invalid-feedback">{confirmPasswordMessage}</div> }
-          </Form.Group>
-          <Button type='submit' variant='outline-primary' disabled={loading}>
-            {loading ? 'Updating Profile...' : 'Update Profile'}
-          </Button>
-        </Form>
-      </Col>
-      <Col md={9}>
-        <h2>My Orders</h2>
-      </Col>
-    </Row>
+    <>
+      <Row className='w-100 mt-4 mb-5' >
+        {/* <div className='d-inline-flex'> */}
+          {/* <h1 className='display-2'>User Profile</h1><h1 className='display-2 mx-4'>|</h1> <h3 className=''>User Profile</h3> */}
+          <Col className='d-inline-flex justify-content-center mb-3' id='userProfileScreenHeading'>
+            <h1 className='display-4'>User Profile</h1><div class="vl mx-4"></div><h3 className=''>User Profile</h3>
+          </Col>
+
+        {/* </div> */}
+      </Row>
+      <Row>
+        <Col md={3}>
+          <h2>User Info</h2>
+          <div>
+            <h5>{user.name}</h5>
+            <h6>{user.email}</h6>
+            <h6>{`Phone Number: ${user.phoneNumber ? user.phoneNumber : `N/A`}`}</h6>
+          </div>
+          {/* <h4>{user.add}</h4> */}
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control 
+                type='name' 
+                // placeholder='Enter name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                // className='is-invalid'
+                className={nameMessage === null ? '' : 'is-invalid'}
+              >
+              </Form.Control>
+              { nameMessage && <div className="invalid-feedback">{nameMessage}</div> }
+            </Form.Group>
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control 
+                type='email' 
+                placeholder='Enter email address' 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={emailMessage === null ? '' : 'is-invalid'}
+              >
+              </Form.Control>
+              { emailMessage && <div className="invalid-feedback">{emailMessage}</div> }
+            </Form.Group>
+            <Form.Group controlId='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type='password' 
+                placeholder='Enter password' 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className={passwordMessage === null ? '' : 'is-invalid'}
+              >
+              </Form.Control>
+              { passwordMessage && <div className="invalid-feedback">{passwordMessage}</div> }
+            </Form.Group>
+            <Form.Group controlId='confirmPassword'>
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control 
+                type='password' 
+                placeholder='Confirm password' 
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className={confirmPasswordMessage === null ? '' : 'is-invalid'}
+              >
+              </Form.Control>
+              { confirmPasswordMessage && <div className="invalid-feedback">{confirmPasswordMessage}</div> }
+            </Form.Group>
+            <Button type='submit' variant='outline-primary' disabled={loading}>
+              {loading ? 'Updating Profile...' : 'Update Profile'}
+            </Button>
+          </Form>
+        </Col>
+        <Col md={9}>
+          <h2>My Orders</h2>
+        </Col>
+      </Row>
+    </>
   )
 }
 
