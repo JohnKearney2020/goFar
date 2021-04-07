870c6e369fd90a775f3684bc4a4337d83c700c5c
John Kearney <John.d.kearney1@gmail.com>
2021-04-06 19:14:30 -0500

    wishlist rows are properly showing out of stock and available in other sizes

--- frontend/src/components/WishListScreen/WishListTableRow.js
+++ frontend/src/components/WishListScreen/WishListTableRow.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Image, Button, Form, Col } from 'react-bootstrap';
+import { Image, Button, Form, Col, Row, Link } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faHeart as solidHeart, faSpinner as spinner } from '@fortawesome/free-solid-svg-icons';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
-// import { faHeart as outlineHeart } from '@fortawesome/free-regular-svg-icons';
-
 
 import { addDecimals } from '../../utilityFunctions/addDecimals';
-import FormContainer from '../FormContainer';
 import Message from '../Message';
 
 const WishListTableRow = ({ productName, color, size, sizeCategory, productImage, dateAdded, index}) => {
@@ -24,8 +20,8 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
   const [qtyForTable, setQtyForTable] = useState(0);
   const [qtyForCart, setQtyForCart] = useState(1);
   const [disableCart, setDisableCart] = useState(false);
-  const [cartErrorMessage, setCartErrorMessage] = useState(false);
-
+  const [availableInOtherSizes, setAvailableInOtherSizes] = useState(false);
+  const [hasSizes, setHasSizes] = useState(false);
 
   // Format the date for the Date column
   const dateObject = new Date(dateAdded);
@@ -41,8 +37,10 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
     // console.log('in wishlist table row useEffect')
     if(wishListProducts.length > 0 && product){
       // Destructure the product object. Doing this outside the useEffect was giving 'undefined' errors
-      const { name, defaultPrice, defaultQty, defaultSalePrice, sizes, hasSizes } = product;
-      console.log(`in wishlist table row useEffect for ${name}`)
+      const { name, defaultPrice, defaultQty, defaultSalePrice, sizes, hasSizes:productHasSizes } = product;
+      if(productHasSizes) { setHasSizes(true) }
+      console.log(`in wishlist table row useEffect for ${name}`);
+
       //=========================================
       //Find the current price and qty available
       //=========================================
@@ -57,8 +55,7 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
         setQtyForTable(defaultQty);
       }
       // Products with sizes - most challenging case
-      if(sizes.length > 0){
-        //Drill down into the product object based on the user's chosen size and color
+      if(sizes.length > 0){ //Drill down into the product object based on the user's chosen size and color
         //In the array of sizes, find the index that corresponds to the size category, i.e. the index for "Regular" or "Tall"
         let levelOne = sizes[sizes.findIndex(i => i.sizeCategoryName === sizeCategory)];
         // console.log(levelOne)
@@ -66,11 +63,25 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
         //Next, find the index in sizeCategoryColorsAndSizes that matches the color the user chose, i.e. "Seapine"
         let levelTwo = levelOne.sizeCategoryColorsAndSizes[levelOne.sizeCategoryColorsAndSizes.findIndex(i => i.color === color)]
         //See if that color is on sale
-        // console.log(levelTwo)
         let colorSalePrice = levelTwo.colorSalePrice;
         //Next, look at the array of sizes in that color and size category and see if the size the customer gave is in stock
         let levelThree = levelTwo.sizeCategorySizes[levelTwo.sizeCategorySizes.findIndex(i => i.size === size)];
         let qtyInStock = levelThree.qty;
+        // console.log('level two:')
+        // console.log(levelTwo)
+        //If there are zero in stock for that size, see if it's in stock in other sizes and size categories
+        if(qtyInStock === 0){
+          let foundAnotherSize = false;
+          setDisableCart(true);
+          //Start at level two, all sizes in that color and size category, and look through all sizes there
+          for(let eachSize of levelTwo.sizeCategorySizes){
+            if(eachSize.qty !== 0){
+              console.log('available in other sizes')
+              setAvailableInOtherSizes(true);
+              break;
+            }
+          }
+        }
         //Update our local state to reflect what we've found
         setQtyForTable(qtyInStock); // For the Qty Available column
         colorSalePrice === 0 ? setTablePrice(addDecimals(sizeCatDefaultPrice)) : setTablePrice(addDecimals(colorSalePrice)); // For the price column
@@ -82,29 +93,11 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
     }
   }, [wishListProducts.length, product, color, size, sizeCategory]);
 
-  const qtyForCartHandler = (e) => {
-    console.log(e.target.value);
-    let newQtyForCart = e.target.value;
-    // if(newQtyForCart > 0 && cartErrorMessage) { setCartErrorMessage(null) };
-    setQtyForCart(newQtyForCart);
-  }
 
   const addToCartHandler = (e) => {
     e.preventDefault();
-    setCartErrorMessage(false);
     console.log('in cart handler')
     console.log(`qty for cart: ${qtyForCart}`)
-    //Form Validation
-    // if(qtyForCart === '0'){ //If the user tries to add quantity zero to their cart
-    //   setCartErrorMessage(true);
-    //   toast.error('Choose a quantity greater than zero...', { position: "top-center", autoClose: 3500 });
-    //   return;
-    // }
-    // if(qtyForCart > qtyForTable){//If the user tries to add more than are in stock
-    //   setCartErrorMessage(true);
-    //   toast.error(`Too many selected. Only ${qtyForTable} in stock.`, { position: "top-center", autoClose: 3500 });
-    //   return;
-    // }
   }
     
   return (
@@ -112,46 +105,28 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
       <td className='tableText'>
         <Image className='tableImage' src={productImage}/>
       </td>
-      {/* <td className='tableText'>{name}</td> */}
       <td className='tableText'>{productName}</td>
       <td className='tableText'>{color}</td>
       <td className='tableText'>{sizeForTable}</td>
-      {/* <td className='tableText'>$65</td> */}
       <td className='tableText'>${tablePrice}</td>
-      {/* <td className='tableText'>{qtyForTable === 0 ? 'Out of Stock' : ( qtyForTable > 10 ? '10+' : qtyForTable )}</td> */}
       <td className='tableText'>{qtyForTable === 0 ? <span className='text-danger font-weight-bold'>Out of Stock</span> : ( qtyForTable > 10 ? '10+' : (qtyForTable <= 5 ? <span className='text-danger font-weight-bold'>{qtyForTable}</span> : qtyForTable ))}</td>
       <td className='tableText'>{dateForTable}</td>
       <td className='tableText'>
-        {/* <FormContainer> */}
-          <Form type='submit' onSubmit={addToCartHandler}>
-            <Form.Row>
-              <Col>
-                {/* <Form.Control 
-                  type='number' 
-                  // size='sm'
-                  // placeholder='Confirm password' 
-                  value={qtyForCart}
-                  // onChange={(e) => setConfirmPassword(e.target.value)}
-                  className={cartErrorMessage === false ? '' : 'is-invalid'}
-                  style={index % 2 === 0 ? { 'backgroundColor': 'white' } :{ 'backgroundColor': '#f7f7f9' }}
-                  disabled={disableCart}
-                  onChange={qtyForCartHandler}
-                >
-                </Form.Control> */}
-                {/* { cartErrorMessage && <div className="invalid-feedback">{cartErrorMessage}</div> }               */}
-                {/* <Form.Control as='select' value={qtyForTable <= 10 ? qtyForTable : 10} onChange={(e) => qtyForCartHandler(e.target.value)} disabled={disableCart}> */}
+        <Form type='submit' onSubmit={addToCartHandler}>
+          <Row className='align-items-center justify-content-center'>
+          {(qtyForCart === 0 && hasSizes === false) && <span className='text-danger font-weight-bold'>Out of Stock</span> } 
+          {(qtyForTable === 0 && hasSizes === true && availableInOtherSizes === true) && <span className='text-danger font-weight-bold'>Available in Other Sizes</span> } 
+          { qtyForTable !== 0 &&
+            <>
+              <Col className='px-0'>
                 <Form.Control 
-                  as='select' 
-                  // value={qtyForTable <= 10 ? qtyForTable : 10} 
-                  defaultValue='test'
+                  as='select'
                   value={qtyForCart} 
                   onChange={(e) => setQtyForCart(e.target.value)} 
-                  // onChange={qtyForCartHandler} 
                   disabled={disableCart}
-                  // style={{"color": "black"}}
+                  className='px-2'
                 >
-                  {[...Array(qtyForTable).keys()].map(x => (
-                    // Limit the user to a max of 10 items added to the cart at once
+                  {[...Array(qtyForTable).keys()].map(x => (// Limit the user to a max of 10 items added to the cart at once
                     (x + 1 <= 10 &&
                       <option key={x+1} value={x + 1}>
                       {x + 1}
@@ -162,13 +137,41 @@ const WishListTableRow = ({ productName, color, size, sizeCategory, productImage
               </Col>
               <Col>
                 <Button size='sm' disabled={disableCart} type='submit'>
-                  <FontAwesomeIcon className='' icon={faCartPlus} size="3x" />
+                  <FontAwesomeIcon className='' icon={faCartPlus} size="2x" />
                 </Button>
               </Col>
-            </Form.Row>
-            {/* {cartErrorMessage && <Message variant='danger'>{cartErrorMessage}</Message>} */}
-          </Form>
-        {/* </FormContainer> */}
+            </>
+          }      
+            {/* {(qtyForCart === 0 && hasSizes === false) ? <span className='text-danger'>Out of Stock</span> : 
+              ((qtyForCart === 0 && hasSizes === true && availableInOtherSizes === true) ? <span className='text-danger'>Available in Other Sizes</span> : 
+                <>
+                  <Col className='px-0'>
+                    <Form.Control 
+                      as='select'
+                      value={qtyForCart} 
+                      onChange={(e) => setQtyForCart(e.target.value)} 
+                      disabled={disableCart}
+                      className='px-2'
+                    >
+                      {[...Array(qtyForTable).keys()].map(x => (// Limit the user to a max of 10 items added to the cart at once
+                        (x + 1 <= 10 &&
+                          <option key={x+1} value={x + 1}>
+                          {x + 1}
+                          </option>
+                        )
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col>
+                    <Button size='sm' disabled={disableCart} type='submit'>
+                      <FontAwesomeIcon className='' icon={faCartPlus} size="2x" />
+                    </Button>
+                  </Col>
+                </>
+              )
+            } */}
+          </Row>
+        </Form>
       </td>
     </tr>
   )
