1868342bcaf366f0e8fcdbfe7712edd904ec9b70
John Kearney <John.d.kearney1@gmail.com>
2021-04-14 23:28:10 -0500

    cart screen warnings now working for out of stock items moved to 'saved for later'. Now Need to put in the actual update cart functionality for the two quantity warnings

--- frontend/src/components/CartScreen/CartMessage.js
+++ frontend/src/components/CartScreen/CartMessage.js
@@ -14,11 +14,12 @@ const CartMessage = ({ variant, itemsChanged, outOfStock }) => {
   return (
     <Alert variant={variant}>
       {!outOfStock && <p className='lead'>The items below in your cart are no longer available in the quantity you originally requested. We've updated your cart with the largest quantity we could give you based on current stock. Checkout soon so you don't miss out!</p>}
-      {outOfStock && <p className='lead'>The items below in your cart are no longer available. We've automatically saved them for later for you.</p>}
+      {outOfStock && <p className='lead'>The items below are no longer available. We've automatically saved them for later for you.</p>}
       <hr />
       <ListGroup>
         {itemsChanged.map((eachItem, Idx) => (
-          <ListGroup.Item key={Idx} className='p-0 cartMessageListItem w-100'>
+          <ListGroup.Item key={Idx} 
+            className={`p-0 w-100 ${variant === 'info' ? `cartMessageListItemInfo` : `cartMessageListItemDanger`}`}>
             <span>
               <span className='font-weight-bold'>{eachItem.name} - </span>
               <span>{eachItem.color} / </span>
