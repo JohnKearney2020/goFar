6620435c0db4e6c013e4b02997bf0f24c430b446
John Kearney <John.d.kearney1@gmail.com>
2021-04-19 14:41:30 -0500

    items in cart can be deleted, moved to saved for later, and moved back to the cart. Moving on to moving to wishlist

--- frontend/src/components/CartScreen/CartRow.js
+++ frontend/src/components/CartScreen/CartRow.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
-import ReactDom from 'react-dom';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { Image, Button, Form, Col, Row, ListGroup } from 'react-bootstrap';
@@ -10,7 +9,6 @@ import { faSpinner as spinner } from '@fortawesome/free-solid-svg-icons';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { USER_LOGIN_SUCCESS } from '../../constants/userConstants';
 import { addDecimals } from '../../utilityFunctions/addDecimals';
-import Message from '../Message';
 
 import './CartRow.css';
 
@@ -26,26 +24,72 @@ const CartRow = ({ productID, name, color, size, sizeCategory, price, qty, image
   price = addDecimals(price);
 
   const userInfo = useSelector(state => state.userLogin.userInfo);
-  const { _id:userID } = userInfo;
+  const { _id:userID, token } = userInfo;
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  }
 
   //Set up local state
   const [loadingDeleteIcon, setLoadingDeleteIcon] = useState(false);
-  
+  const [updatingCartIcon, setUpdatingCartIcon] = useState(false);
+  const [savingForLaterIcon, setSavingForLaterIcon] = useState(false);
+  const [movingToWishlistIcon, setMovingToWishlistIcon] = useState(false);
+  const [movingToCartIcon, setMovingToCartIcon] = useState(false);
 
+  //This moves items to saved for later or back to the cart
+  const moveInCartHandler = async (e) => {
+    let savedForLaterBoolean = e.target.value;
+    console.log(`e.target.value: ${e.target.value}`)
+    setSavingForLaterIcon(true);
+    setMovingToCartIcon(true);
+    try {
+      //attempt to move the item to the user's Cart OR move it to saved for later
+      const { data } = await axios.put('/api/users/cart/savedforlater', {
+        productID, 
+        name: name,
+        color,
+        size,
+        sizeCategory,
+        savedForLater: savedForLaterBoolean
+      }, config);
+      toast.info(`${name} - ${color} / ${size} / ${sizeCategory} was moved to ${savedForLaterBoolean.toString() === 'true' ? `Save for Later` : `your Cart`}`, { position: "bottom-center", autoClose: 4000 });
+      // We've set up the backend to send us back the updated user information once the user's cart is updated. We need to 
+      // dispatch the user login again to update the user's cart in the global state
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: data
+      });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+      console.log('there was an error')
+      console.log(error)
+      toast.error(`Could not add ${name} to your cart. Try again later.`, { position: "top-right", autoClose: 4000 });
+      setUpdatingCartIcon(false);
+      setMovingToCartIcon(false);
+    } 
+  }
+
+  const moveToWishlistHandler = async () => {
+    setMovingToWishlistIcon(true);
+    setUpdatingCartIcon(true);
+    console.log('clicked move to wishlist button')
+    setTimeout(() => {
+      toast.info(`${name} - ${color} / ${size} / ${sizeCategory} was moved to your Wishlist`, { position: "bottom-center", autoClose: 4000 });
+      setMovingToWishlistIcon(false);
+      setUpdatingCartIcon(false);
+    }, 2000);
+  }
+  
   const deleteCartItemHandler = async () => {
     setLoadingDeleteIcon(true);
-    // deleteButtonClicked.current = true;
+    setUpdatingCartIcon(true);
     console.log('delete from cart clicked')
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${userInfo.token}`
-      }
-    }
     try {
       const { data } = await axios.delete(`/api/users/cart/cartitem/${userID}&${productID}&${color}&${size}&${sizeCategory}`, config);
-      // setLoadingDeleteIcon(false);
-      console.log(data);
       dispatch({
         type: USER_LOGIN_SUCCESS,
         payload: data
@@ -57,6 +101,7 @@ const CartRow = ({ productID, name, color, size, sizeCategory, price, qty, image
       console.log(error)
       toast.error(`Could not delete that item from your cart. Try again later.`, { position: "top-right", autoClose: 3500 });
       setLoadingDeleteIcon(false);
+      setUpdatingCartIcon(false);
     }
   }
 
@@ -122,14 +167,26 @@ const CartRow = ({ productID, name, color, size, sizeCategory, price, qty, image
           {qty}
         </Col> */}
         <Col md={1} className='d-flex justify-content-center'>
-          <Button size='sm' variant='danger' className='' disabled={loadingDeleteIcon} onClick={deleteCartItemHandler}>
+          <Button size='sm' variant='danger' className='' disabled={updatingCartIcon} onClick={deleteCartItemHandler}>
             <FontAwesomeIcon className='' icon={loadingDeleteIcon ? spinner : faTrashAlt} size="2x"/>
           </Button>
         </Col>
       </Row>
       <Row className='justify-content-start mt-2 ml-1'>
-        {savedForLater === true ? <Button className='p-0 px-2' variant="secondary">Move to Cart</Button> : <Button className='p-0 px-2' variant="secondary">Save for Later</Button>}|
-        <Button className='p-0 px-2 cartRowButton' variant="secondary">Move to Wishlist</Button>
+        {savedForLater === true ?
+          // Move to cart button
+          <Button className='p-0 px-2' variant="secondary" disabled={updatingCartIcon} onClick={moveInCartHandler} value={false}>
+            {movingToCartIcon ? <span><FontAwesomeIcon className='' icon={spinner} /> Moving... </span> : `Move to Cart`}
+          </Button> : 
+          // Save for later button
+          <Button className='p-0 px-2' variant="secondary" disabled={updatingCartIcon} onClick={moveInCartHandler} value={true}>
+              {savingForLaterIcon ? <span><FontAwesomeIcon className='' icon={spinner} /> Saving... </span> : `Save for Later`}
+          </Button>
+        }|
+        {/* Move to wishlist button */}
+        <Button className='p-0 px-2 cartRowButton' variant="secondary" disabled={updatingCartIcon} onClick={moveToWishlistHandler}>
+          {movingToWishlistIcon ? <span><FontAwesomeIcon className='' icon={spinner} /> Moving... </span> : `Move to Wishlist`}
+        </Button>
       </Row>
     </ListGroup.Item>
   </>
