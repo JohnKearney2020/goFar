2966c9868411e98f156c51ea959144af16e3f336
John Kearney <John.d.kearney1@gmail.com>
2021-05-10 13:24:30 -0500

    happy with up and down carets on click

---
+++ frontend/src/components/HeaderAndFooter/BigNavDropdown.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './BigNavDropdown.css';
+
+const BigNavDropdown = () => {
+  return (
+    <div id='bigNavDropDown'>
+      
+    </div>
+  )
+}
+
+export default BigNavDropdown
