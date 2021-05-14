2ef6fb75e498ac00f2c5f0915b56b322fa9852ff
John Kearney <John.d.kearney1@gmail.com>
2021-05-14 12:59:43 -0500

    making a branch for geocoding during order creation

--- frontend/src/components/ProfileScreen/OrderMap.js
+++ frontend/src/components/ProfileScreen/OrderMap.js
@@ -9,24 +9,25 @@ const OrderMap = ({ address, zoom }) => {
   const addressForMap = `${line1} ${line2 ? line2 : ''} ${city}, ${state} ${zipCode}`;
 
   useEffect(() => {
-    const geocoder = new window.google.maps.Geocoder();
+    if(window.google){
+      const geocoder = new window.google.maps.Geocoder();
 
-    const map = new window.google.maps.Map(mapRef.current, {
-      zoom: zoom
-    });
-  
-    geocoder.geocode( { 'address': addressForMap}, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new window.google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-
+      const map = new window.google.maps.Map(mapRef.current, {
+        zoom: zoom
+      });
+    
+      geocoder.geocode( { 'address': addressForMap}, function(results, status) {
+        if (status == 'OK') {
+          map.setCenter(results[0].geometry.location);
+          var marker = new window.google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+          });
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+    }
     return () => {
       
     }
