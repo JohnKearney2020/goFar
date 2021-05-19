4ff649e305b0a771007b8f3065c721059e8f9990
John Kearney <John.d.kearney1@gmail.com>
2021-05-18 22:17:41 -0500

    trying heroku https middleware

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,21 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 //===============================================
+//         Heroku https middleware
+//===============================================
+// Heroku lets users access our site by http:// for some reason, alongside https://
+// This gives a security warning in chrome, among other things
+// This middleware redirects all http: requests to https:
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
+//===============================================
 //         Parse JSON from Body
 //===============================================
 app.use(express.json()); //this will allow us to accept JSON data in the body
