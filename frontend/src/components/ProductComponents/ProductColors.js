c5580c91b5c40ada045916a6625975774dfb263d
John Kearney <John.d.kearney1@gmail.com>
2021-06-09 20:55:08 -0500

    added hand cursor over color buttons on the various product pages

--- frontend/src/components/ProductComponents/ProductColors.js
+++ frontend/src/components/ProductComponents/ProductColors.js
@@ -6,14 +6,6 @@ import './ProductColors.css';
 
 const ProductColors = ({ images, colorSelectHandler, productColorsArray, colorFromUrl }) => {
 
-  // const fromHomeScreen = () => {
-  //   for(let eachColor of colorsButtons) {
-  //     eachColor.classList.remove('colorButtonActiveBorder');
-  //   }
-  // }
-  // console.log('in ProductColors component');
-  // console.log(images)
-
   const newColorClickedHandler = (e) => {
     colorSelectHandler(e.target.dataset.color); //its expecting a color name as a string, ex: 'seapine'
     const colorsButtons = document.getElementsByClassName('colorButton');
@@ -25,35 +17,27 @@ const ProductColors = ({ images, colorSelectHandler, productColorsArray, colorFr
   }
 
   return (
-      <>
-        {productColorsArray.length === 1 ?
-          <Image src={productColorsArray[0].tinyImage} roundedCircle thumbnail className="mr-1 colorButton colorButtonActiveBorderPermanent" 
-          key={productColorsArray[0].tinyImage}
-          data-color={productColorsArray[0].colorName}
-          id={uuidv4()}
-          /> :
-          images.map((eachColorImage,idx) => (
-            <Image src={eachColorImage.tinyImage} 
-            roundedCircle 
-            thumbnail 
-            className={`mr-1 colorButton ${eachColorImage.colorName === colorFromUrl ? 'colorButtonActiveBorder' : ''}`}
-            // className={`mr-1 colorButton`}
-            key={eachColorImage.tinyImage}
-            data-color={eachColorImage.colorName}
-            onClick={newColorClickedHandler}
-            id={uuidv4()}
-            />
-          ))
-        }
-        {/* {images.map((eachColorImage,idx) => (
-          <Image src={eachColorImage.tinyImage} roundedCircle thumbnail className="mr-1 colorButton" 
+    <>
+      {productColorsArray.length === 1 ?
+        <Image src={productColorsArray[0].tinyImage} roundedCircle thumbnail className="mr-1 colorButton colorButtonActiveBorderPermanent" 
+        key={productColorsArray[0].tinyImage}
+        data-color={productColorsArray[0].colorName}
+        id={uuidv4()}
+        /> :
+        images.map((eachColorImage,idx) => (
+          <Image src={eachColorImage.tinyImage} 
+          roundedCircle 
+          thumbnail 
+          className={`mr-1 colorButton ${eachColorImage.colorName === colorFromUrl ? 'colorButtonActiveBorder' : ''}`}
+          // className={`mr-1 colorButton`}
           key={eachColorImage.tinyImage}
           data-color={eachColorImage.colorName}
-          onClick={productColorsArray ? (productColorsArray.length > 1 ? newColorClickedHandler : null) : null}
+          onClick={newColorClickedHandler}
           id={uuidv4()}
           />
-        ))} */}
-      </>
+        ))
+      }
+    </>
   )
 }
 
