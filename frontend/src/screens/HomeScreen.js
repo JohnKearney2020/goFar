0468a58ae6a6004ceb3600b8d7f7a01338b7d263
John Kearney <John.d.kearney1@gmail.com>
2021-07-10 12:50:48 -0500

    added fade in transition for the homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
+import { CSSTransition } from 'react-transition-group';
 
 import { listProducts, listGenderProducts } from '../actions/productActions';
 import ProductCard from '../components/ProductComponents/ProductCard';
@@ -16,12 +17,13 @@ const HomeScreen = ({ match }) => {
   const gender = match.params.gender;
   const pageNumber = match.params.pageNumber || 1;
 
+  // Get products from the global state
   const dispatch = useDispatch();
   const productList = useSelector(state => state.productList);
   const { loading, error, products, page, pages } = productList;
-  
-  const noResultsMessage = `No results. We couldn't find any products that matched what you were looking for. Try another search and we will do our best to find them. Pinky promise :)
-  `
+
+  const noResultsMessage = `No results. We couldn't find any products that matched what you were looking for. Try another search and we will do our best to find them. Pinky promise :)`
+
   useEffect(() => {
     if(keyword === 'all'){
       dispatch(listGenderProducts(gender, pageNumber));
@@ -30,26 +32,35 @@ const HomeScreen = ({ match }) => {
     }
   }, [dispatch, keyword, gender, pageNumber]);
 
+  let productsToDisplay = products.map((product, idx) => {
+    return (
+      <Col className='d-flex align-items-stretch' key={product._id} sm={12} md={6} lg={4} xl={3}>
+        <ProductCard product={product} key={product._id}/>
+      </Col>
+    )
+  })
+
   return (
-    <>
-      <Helmet>
-        <title>{`Welcome to Go Far`}</title>
-      </Helmet>
-    {/* <Message variant='info'>Hello! Thank you for checking out my site. As of 5/18/2021 this is still a work in progress that I hope to finish soon! Check back soon for added functionality</Message> */}
-    {gender && keyword !== 'all' ? <h1>{gender}'s {keyword}</h1> : keyword && keyword !== 'all' ? <h1>Search Results for "{keyword}"...</h1> : keyword === 'all' ? <h1>{gender}'s Clothing</h1> : <h1>Latest Products</h1>}
-      {loading ? ( <Loader /> ) : error ? ( <Message variant='danger'>{error}</Message> ) 
-        : products.length === 0 ? ( <Message variant='info'>{noResultsMessage}</Message> ) :
-        <>
-          <Row>
-            {products.map(product => (
-              <Col className='d-flex align-items-stretch' key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <ProductCard product={product} key={product._id}/>
-              </Col>
-            ))}
-          </Row>
-          <Paginate page={page} pages={pages} keyword={keyword ? keyword : ''} gender={gender ? gender : ''} />
-        </>
-      }
+      <>
+        <Helmet>
+          <title>{`Welcome to Go Far`}</title>
+        </Helmet>
+        {gender && keyword !== 'all' ? <h1>{gender}'s {keyword}</h1> : keyword && keyword !== 'all' ? <h1>Search Results for "{keyword}"...</h1> : keyword === 'all' ? <h1>{gender}'s Clothing</h1> : <h1>Latest Products</h1>}
+        {loading && <Loader />}
+        {error && <Message variant='danger'>{error}</Message> }
+        {products.length === 0 && !loading && <Message variant='info'>{noResultsMessage}</Message> }
+          <CSSTransition 
+            in={productsToDisplay.length === products.length && productsToDisplay.length > 0} 
+            timeout={2000} 
+            classNames="product-transition"
+          >
+            <div>
+              <Row>
+                {productsToDisplay}
+              </Row>
+              <Paginate page={page} pages={pages} keyword={keyword ? keyword : ''} gender={gender ? gender : ''} />
+            </div>
+          </CSSTransition>
     </>
   )
 }
